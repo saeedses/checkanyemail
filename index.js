4155d2cb5b04a6e17e80cdbe7a9a5d4dbf981763
Saeed Ehsann <saeed@dev01.polyvista.net>
2016-11-13 01:06:24 +0500

    ddsss sdsd

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var app = express();
 var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // views is directory for all template files
 var router = express.Router();              // get an instance of the express Router
-
+console.log('ss');
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/:email_id', function(req, res) {
 var response = '';
@@ -31,7 +30,7 @@ console.log(resp);
   });
     
 });
-
+app.use('/api', router);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
